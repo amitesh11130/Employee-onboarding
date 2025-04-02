abcb8027ed6c0adf5e28d10b140c4a34a7b66c1c
unknown <amitesh11130@gmail.com>
2025-04-02 13:50:21 +0530

    adding test case

--- src/test/java/com/springbootapi/EmployeeEmployeeOnboardingApplicationTests.java
+++ src/test/java/com/springbootapi/EmployeeEmployeeOnboardingApplicationTests.java
@@ -1,13 +1,21 @@
 package com.springbootapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@Slf4j
 class EmployeeEmployeeOnboardingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Test case executing..");
+		assertEquals(true,true);
 	}
+
+
 
 }
